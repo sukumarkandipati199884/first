8bc374fc3e7d035b02ff212f20929b17fbde7cc1
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-21 22:06:19 +0200

    AutoDeploy.AI update Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Healthcare Appointment Booking App</h1>
+      <p>Find and book appointments with doctors easily.</p>
+    </div>
+  );
+}
+
+export default Home;
