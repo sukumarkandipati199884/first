f24bbcce3e3025f93999539a70f69b159df6bca8
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-24 23:33:23 +0200

    AutoDeploy.AI update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Admin Dashboard
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <nav className="navbar">
+      <h1>Portfolio</h1>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
   );
 }
 
