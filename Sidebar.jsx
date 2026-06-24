f2b4651421d761b1dc2529ca1ae435b10c6507e7
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-24 23:18:46 +0200

    AutoDeploy.AI update Sidebar.jsx

---
+++ Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>Sidebar</h2>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
