529c1a3060c343588d347be74f11bbbaa7660c41
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 01:43:31 +0200

    AutoDeploy.AI update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import KPISection from './components/KPISection';
-import AnalyticsSection from './components/AnalyticsSection';
-import UserTable from './components/UserTable';
-import ActivityFeed from './components/ActivityFeed';
 import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Sidebar />
-      <Container maxWidth="lg">
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={4}>
-            <KPISection />
-          </Grid>
-          <Grid item xs={12} md={6} lg={8}>
-            <AnalyticsSection />
-          </Grid>
-          <Grid item xs={12}>
-            <UserTable />
-          </Grid>
-          <Grid item xs={12}>
-            <ActivityFeed />
-          </Grid>
-        </Grid>
-      </Container>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
