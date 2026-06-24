87222fba26b256c87d384630dd0448c8afd6031c
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-24 23:33:27 +0200

    AutoDeploy.AI update About.jsx

---
+++ About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>This is the about section.</p>
+    </section>
+  );
+}
+
+export default About;
