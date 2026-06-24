dd26e460dc3a35dda28f070ecb770a5efc29eb89
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-24 23:34:52 +0200

    AutoDeploy.AI update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -4,7 +4,6 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>Portfolio</h1>
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
