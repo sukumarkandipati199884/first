46401352cad3920f00e9cfa764943d61dc4bfb0a
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 05:51:54 +0200

    AutoDeploy.AI update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { CssBaseline, Container } from '@mui/material';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Transactions from './components/Transactions';
-import Profile from './components/Profile';
 import './App.css';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import DashboardContent from './components/DashboardContent';
 
 function App() {
   return (
-    <Router>
-      <CssBaseline />
+    <div className="App">
       <Navbar />
-      <Container maxWidth="lg">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Container>
-    </Router>
+      <div className="main-layout">
+        <Sidebar />
+        <DashboardContent />
+      </div>
+    </div>
   );
 }
 
