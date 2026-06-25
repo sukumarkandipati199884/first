a884bda086018a963d6ee2d3800b8af431446de6
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 05:50:55 +0200

    AutoDeploy.AI update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { TextField, Button, Box } from '@mui/material';
 
-const Login = () => {
+function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Please log in to access the dashboard.</p>
-    </div>
+    <Box sx={{ mt: 8 }}>
+      <TextField label="Username" variant="outlined" fullWidth margin="normal" />
+      <TextField label="Password" type="password" variant="outlined" fullWidth margin="normal" />
+      <Button variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
+        Login
+      </Button>
+    </Box>
   );
-};
+}
 
 export default Login;
