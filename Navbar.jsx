9f9a1b41ca570a46f4473057a0bbd8755f52c1ae
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 05:51:56 +0200

    AutoDeploy.AI update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Banking App
-        </Typography>
-        <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
-        <Button color="inherit" component={Link} to="/transactions">Transactions</Button>
-        <Button color="inherit" component={Link} to="/profile">Profile</Button>
-      </Toolbar>
-    </AppBar>
+    <nav className="navbar">
+      <h1>Dashboard</h1>
+    </nav>
   );
 }
 
