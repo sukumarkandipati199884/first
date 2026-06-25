95a6d4ac2f883fd2465fcdf81434088a59ba950c
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 08:07:04 +0200

    AutoDeploy.AI update transactions_screen.dart

---
+++ transactions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transactions'),
+      ),
+      body: Center(
+        child: Text('List of Transactions'),
+      ),
+    );
+  }
+}
