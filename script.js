b1a1f9dc7a934dd906578c055fed82ed759b6be7
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 04:12:23 +0200

    AutoDeploy.AI update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const targetId = this.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
             window.scrollTo({
-                top: targetSection.offsetTop - 60,
+                top: targetSection.offsetTop - 70,
                 behavior: 'smooth'
             });
         });
