827383196754f10d6ee0d34dfcbb732e6e21884c
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 05:44:55 +0200

    AutoDeploy.AI update Users.jsx

---
+++ Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>Manage your users here.</p>
+    </div>
+  );
+};
+
+export default Users;
