c290ae917e3b8d46dbce51e4f3d3f92d2a932f1f
sukumar kandipati <sukumar.kandipati@ue-germany.de>
2026-06-25 05:44:56 +0200

    AutoDeploy.AI update Login.jsx

---
+++ Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Please log in to access the dashboard.</p>
+    </div>
+  );
+};
+
+export default Login;
